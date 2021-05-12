28cca5c08dec289a5cf12209c721446bcc890f9b
drinel <will.i.am.002@hotmail.com>
2021-05-12 14:42:35 -0300

    Package controller changed location

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -1,4 +1,4 @@
-package controller;
+package one.digitalinnovation.personapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
